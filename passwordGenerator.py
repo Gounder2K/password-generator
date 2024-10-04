6629b68bc79bc122ba990377f934324dc15bb1af
PrateekG2000 <>
2024-10-04 21:29:16 +1300

    Adding Symbols and Capitalising random letters

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,12 +1,29 @@
 import string
 import random
-
-def main():
+def generateRandomPassword(n,symbols = 'N',capital = 'N'):
     password = ''
-    numChar = input("How many characters do you want?: ")
-    for i in range(0,int(numChar)) :
+    for i in range(0,n):
         password = password + random.choice(string.ascii_letters)
     print(password)
+    if symbols == 'Y':
+        randInd = random.randrange(0,n-1)
+        temp = random.choice(string.punctuation)
+        password = password[:randInd-1]+ temp + password[randInd:]
+        
+    if capital == 'Y':
+        randInd = random.randrange(0,n-1)
+        password =''.join(c.upper() if i == randInd else c for i, c in enumerate(password))
+
+    return password
+        
+
+def main():
+    numChar = input("How many characters do you want?: ")
+    symbols = input("Do you want to include symbols?: Y or N ")
+    capital = input("Do you require a capital letter?: Y or N ")
+    password = generateRandomPassword(int(numChar),symbols,capital)
+
+    
 
 if __name__ == "__main__":
     main()
